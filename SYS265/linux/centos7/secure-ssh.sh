ea1980eef76fbdea2ac785d3e041ba8b763bd2af
dbelair-champl <54853006+dbelair-champl@users.noreply.github.com>
2020-02-17 11:54:38 -0500

    Update secure-ssh.sh

--- SYS265/linux/centos7/secure-ssh.sh
+++ SYS265/linux/centos7/secure-ssh.sh
@@ -5,6 +5,7 @@
 #removes roots ability to ssh in
 sudo useradd -m -d /home/$1 -s /bin/bash $1
 sudo mkdir /home/$1/.ssh
+sudo mkdir /home/$1/.ssh/authorized_keys
 sudo cp SYS265/linux/public-keys/id_rsa.pub /home/$1/.ssh/authorized_keys
 sudo chmod 700 /home/$1/.ssh
 sudo chmod 600 /home/$1/.shh/authorized_keys
