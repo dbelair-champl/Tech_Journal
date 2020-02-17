f32c3488fc7ce5a4e9bb08f350a858c4652a4e52
dbelair-champl <54853006+dbelair-champl@users.noreply.github.com>
2020-02-17 11:44:34 -0500

    Update secure-ssh.sh

--- SYS265/linux/centos7/secure-ssh.sh
+++ SYS265/linux/centos7/secure-ssh.sh
@@ -6,6 +6,6 @@
 echo "sudo useradd -m -d /home/$1 -s /bin/bash $1
 sudo mkdir /home/$1/.ssh
 sudo cp SYS265/linux/public-keys/id_rsa.pub /home/$1/.ssh/authorized_keys
-sudo chmod 100 /home/$1/.ssh
+sudo chmod 700 /home/$1/.ssh
 suod chmod 600 /home/$1/.shh/authorized_keys
 sudo chown -R $1:$1 /home/$1/.ssh"
