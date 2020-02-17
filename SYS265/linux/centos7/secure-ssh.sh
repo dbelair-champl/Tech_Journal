065e3fd2370b3f8a7fa9ce47ed1fc66adbefbd65
dbelair-champl <54853006+dbelair-champl@users.noreply.github.com>
2020-02-17 11:28:21 -0500

    Update secure-ssh.sh

--- SYS265/linux/centos7/secure-ssh.sh
+++ SYS265/linux/centos7/secure-ssh.sh
@@ -1,5 +1,5 @@
 #secure-shh.sh
-#auther dbelair
+#author dbelair
 #creates a new ssh user using $1 parameter
 #adds a public key from the local repo or curled from the remote repo
 #removes roots ability to ssh in
