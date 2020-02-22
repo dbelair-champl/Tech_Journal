52c00738ecf35c6e5a0f974c6ac3383095cbf2e9
dbelair-champl <54853006+dbelair-champl@users.noreply.github.com>
2020-02-22 18:03:54 -0500

    Update secure-ssh.sh

--- SYS265/linux/centos7/secure-ssh.sh
+++ SYS265/linux/centos7/secure-ssh.sh
@@ -4,6 +4,7 @@
 #adds a public key from the local repo or curled from the remote repo
 #removes roots ability to ssh in
 sudo useradd -m -d /home/$1 -s /bin/bash $1
+sudo mkdir /home/$1
 sudo mkdir /home/$1/.ssh
 sudo cp SYS265/linux/public-keys/id_rsa.pub /home/$1/.ssh/authorized_keys
 sudo chmod 700 /home/$1/.ssh
